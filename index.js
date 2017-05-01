7b382cac9bf27ed209a9dd293dfaf3d57e4707cb
Maxime Guedj <maxime@guedjdev.com>
2017-05-01 19:51:33 +0000

    Add some functions

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    algoliaHelper: require('./lib/algolia-helper'),
+    errors: require('./lib/error.const'),
+    errorSchema: require('./lib/error.schema'),
+    logger: require('./lib/logger'),
+    utils: require('./lib/utils')
+};
